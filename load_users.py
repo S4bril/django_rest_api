0daec0d17d3f246c05cf7c6ac012dcf1c9553b0a
S4bril <fszczepanski48@gmail.com>
2025-01-09 21:12:12 +0000

    Add creating location for each user in load_users.py script

--- load_users.py
+++ load_users.py
@@ -3,6 +3,9 @@ import sys
 import json
 import django
 
+from tqdm import tqdm
+from termcolor import colored
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'foundyou_api')))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
@@ -10,6 +13,8 @@ django.setup()
 from app.models import CustomUser, Location
 
 JSON_FILE = "app/json_forms/users.json"
+POSITIVE_COLOR = "green"
+NEGATIVE_COLOR = "red"
 
 def load_users_from_json(file_path):
     try:
@@ -23,44 +28,46 @@ def load_users_from_json(file_path):
         print(f"Error: File {file_path} contains invalid JSON.")
         return []
 
-def add_users_to_db(users):
-    for user_data in users:
-        try:
-            location_data = {
-                "latitude": user_data["location"][0],
-                "longitude": user_data["location"][1]
-            }
-            print(location_data)
-            location, _ = Location.objects.get_or_create(**location_data)
-            user = CustomUser.objects.get(email=user_data["email"])
-            user.location = location
-            user.save()
-            saved_user = CustomUser.objects.get(email=user_data["email"])
-            if saved_user.location == location:
-                print(f"Successfully updated location for user: {saved_user.email}")
-            else:
-                print(f"Failed to update location for user: {saved_user.email}")
-        except Exception as e:
-            print(f"Error adding location {user_data.get('location')}: {e}")
-        # try:
-        #     user = CustomUser(
-        #         username=user_data["username"],
-        #         email=user_data["email"],
-        #         sex=user_data["sex"],
-        #         birthday=user_data["birthday"],
-        #         bio=user_data["bio"],
-        #         passions=user_data["passions"]
-        #     )
-        #     user.set_password(user_data["password"])
-        #     user.save()
+def add_location_to_user(user, user_data):
+    try:
+        location_data = {
+            "latitude": user_data["location"][0],
+            "longitude": user_data["location"][1]
+        }
+        location = Location.objects.create(**location_data)
+        user.location = location
+        user.save()
+    except Exception as e:
+        print(colored(f"Error adding location {user_data.get('location')}: {e}", NEGATIVE_COLOR))
 
-        #     print(f"Added user: {user.username}")
-        # except Exception as e:
-        #     print(f"Error adding user {user_data.get('username')}: {e}")
+def add_users_to_db(users):
+    added_users = 0
+    for user_data in tqdm(users, desc="Adding users to DB"):
+        try:
+            user = CustomUser(
+                username=user_data["username"],
+                email=user_data["email"],
+                sex=user_data["sex"],
+                birthday=user_data["birthday"],
+                bio=user_data["bio"],
+                passions=user_data["passions"]
+            )
+            user.set_password(user_data["password"])
+            user.save()
+            add_location_to_user(user, user_data)
+            if CustomUser.objects.get(email=user_data["email"]):
+                added_users += 1
+
+        except Exception as e:
+            print(colored(f"Error adding user {user_data.get('username')}: {e}", NEGATIVE_COLOR))
+    return added_users
 
 if __name__ == "__main__":
     users = load_users_from_json(JSON_FILE)
+    added_users = 0
+    print(f"{len(users)} users to add")
     if users:
-        add_users_to_db(users)
+        added_users = add_users_to_db(users)
     else:
         print("No users to add.")
+    print(colored(f"Job is done. Number of added users: {added_users}", POSITIVE_COLOR))
