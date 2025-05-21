3730d97670d4a910e56feaf470e76271860cb365
S4bril <95976169+S4bril@users.noreply.github.com>
2025-05-22 00:04:48 +0200

    32 hotfix fix creating new user (#33)
    * Change serializer
    
    * Fix tests

--- fu_api/features/users/views.py
+++ fu_api/features/users/views.py
@@ -1,11 +1,12 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from fu_api.features.common.serializers.friend_serializers import FriendSerializer
+from fu_api.features.user_profile.serializers import CustomUserSerializer
 from fu_api.models.custom_user_model import CustomUser
 
 
 class UsersListCreateView(ListCreateAPIView):
-    serializer_class = FriendSerializer
+    serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
     permission_classes = [AllowAny]
 
