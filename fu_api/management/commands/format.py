c412e142d022fd7de16616b5e4fb87f1b003bbab
S4bril <95976169+S4bril@users.noreply.github.com>
2025-05-30 18:42:40 +0200

    Fix formatting

--- fu_api/management/commands/format.py
+++ fu_api/management/commands/format.py
@@ -6,7 +6,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
-            subprocess.run(["isort", "."], check=True)
+            subprocess.run(["isort", "--profile", "black", "."], check=True)
             subprocess.run(["black", "."], check=True)
         except subprocess.CalledProcessError as e:
             self.stderr.write(self.style.ERROR(e))
