c630912bbc2c7e47141af8a466396bdbc364ce73
S4bril <filip.informatyka@vp.pl>
2025-06-23 01:35:11 +0200

    Fix

--- fu_api/models/custom_user_model.py
+++ fu_api/models/custom_user_model.py
@@ -1,9 +1,10 @@
 import os
-
+from datetime import date
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.timezone import now
 
+from .location_model import Location
 
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=150, blank=False, null=False)
@@ -15,19 +16,20 @@ class CustomUser(AbstractUser):
         ],
         blank=False,
         null=False,
+        default=0,
     )
-    birthday = models.DateField(blank=False, null=False)
-    bio = models.TextField(blank=False, null=False)
+    birthday = models.DateField(blank=False, null=False, default=date.today)
+    bio = models.TextField(blank=False, null=False, default='')
     created_at = models.DateTimeField(default=now)
     profile_image = models.ImageField(
         upload_to="profile_images/", blank=True, null=True
     )
     blocked_users = models.ManyToManyField("self", blank=True, symmetrical=False)
     rejected_users = models.ManyToManyField("self", blank=True, symmetrical=True)
-    bio_embedding = models.JSONField(null=False, blank=False)
+    bio_embedding = models.JSONField(null=False, blank=False, default=dict)
 
     location = models.OneToOneField(
-        "Location",
+        Location,
         related_name="user",
         on_delete=models.SET_NULL,
         null=True,
@@ -46,7 +48,7 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return f"{self.username}"
 
-    def delete(self, *args, **kwargs):  # maybe do this with signals
+    def delete(self, *args, **kwargs):
         if self.profile_image:
             if os.path.isfile(self.profile_image.path):
                 os.remove(self.profile_image.path)
